6eead5c3a44b277b62f08414fee79d561a901c25
Brice Schaffner <brice.schaffner@swisstopo.ch>
2021-06-08 14:53:07 +0200

    BGDIINF_SB-1304: Uses new bod views from service-wmts schema
    A new schema on the BOD has been created for the views for this service.
    The views are based on the existing views in re3 schemas with the
    following changes:
    
    - Uses only english column name
    - Fallback all rm to de when no romanisch translation is available (in re3 not all column had a fallback to de)
    - Uses array instead of comma separated strings with joins
    - view_wmts_getcapabilities_xx
      - removed following unused column
        - inspire_xxx
        - geobasisdatensatz_name
        - datenherr
        - wms_kontakt_abkuerzung
        - wms_kontakt_name
        - chargeable
      - removed the joins to the re3.catalog and xt_dataset_wms
    - view_wmts_service_metadata_xx
     - removed following unused column
       - onlineresource
       - encoding

--- app/models.py
+++ app/models.py
@@ -20,17 +20,17 @@ class QueryGetCap(BaseQuery):  # pylint: disable=too-many-ancestors
             'prod': self.filter(GetCap.staging == staging)
         }[staging]
 
-    def filter_by_map(self, map_name):
-        if map_name != 'all':
+    def filter_by_topic(self, topic_name):
+        if topic_name != 'all':
             clauses = []
-            if map_name == 'api':
-                clauses.append(GetCap.maps.like(f'%{map_name.lower()}%'))
+            if topic_name == 'api':
+                clauses.append(GetCap.topics.like(f'%{topic_name.lower()}%'))
             else:
                 # we also want to always include all 'ech' layers
                 # (except for api's)
-                clauses.append(GetCap.maps.like(f'%{map_name.lower()}%'))
+                clauses.append(GetCap.topics.like(f'%{topic_name.lower()}%'))
                 # whitelist hack
-                clauses.append(GetCap.maps.like(r'%ech%'))
+                clauses.append(GetCap.topics.like(r'%ech%'))
             return self.filter(or_(*clauses))
         return self
 
@@ -38,58 +38,48 @@ class QueryGetCap(BaseQuery):  # pylint: disable=too-many-ancestors
 class GetCap(object):
 
     id = db.Column('fk_dataset_id', db.Unicode, primary_key=True)
-    formats = db.Column('format', db.Unicode)
-    timestamps = db.Column('timestamp', db.Unicode)
-    bod_layer_id = db.Column('bod_layer_id', db.Unicode)
+    id_geocat = db.Column('id_geocat', db.Unicode)
     staging = db.Column('staging', db.Unicode)
-    description = db.Column('bezeichnung', db.Unicode)
-    short_description = db.Column('kurzbezeichnung', db.Unicode)
+    description = db.Column('description', db.Unicode)
+    short_description = db.Column('short_description', db.Unicode)
     abstract = db.Column('abstract', db.Unicode)
-    inspire_name = db.Column('inspire_name', db.Unicode)
-    inspire_abstract = db.Column('inspire_abstract', db.Unicode)
-    inspire_upper_theme_name = db.Column('inspire_oberthema_name', db.Unicode)
-    inspire_upper_theme_abstract = db.Column(
-        'inspire_oberthema_abstract', db.Unicode
-    )
-    geo_base_data_set_name = db.Column('geobasisdatensatz_name', db.Unicode)
-    data_owner = db.Column('datenherr', db.Unicode)
-    wms_contact_abbreviation = db.Column('wms_kontakt_abkuerzung', db.Unicode)
-    wms_contact_name = db.Column('wms_kontakt_name', db.Unicode)
-    resolution_min = db.Column('resolution_min', db.Integer)
-    resolution_max = db.Column('resolution_max', db.Integer)
-    maps = db.Column('topics', db.Unicode)  # the topics
-    chargeable = db.Column('chargeable', db.Boolean)
-    id_geocat = db.Column('idgeocat', db.Unicode)
-    has_legend = False  # TODO uses the hasLegend once it has been added
+    formats = db.Column('formats', db.ARRAY(db.Unicode))
+    timestamps = db.Column('timestamps', db.ARRAY(db.Unicode))
+    resolution_min = db.Column('resolution_min', db.Float)
+    resolution_max = db.Column('resolution_max', db.Float)
+    s3_resolution_max = db.Column('s3_resolution_max', db.Float)
+    cache_ttl = db.Column('cache_ttl', db.Float)
+    topics = db.Column('topics', db.ARRAY(db.Unicode))
+    has_legend = db.Column('has_legend', db.Boolean)
 
 
 class GetCapFr(db.Model, GetCap):
-    __tablename__ = 'view_bod_wmts_getcapabilities_fr'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_fr'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
     query_class = QueryGetCap
 
 
 class GetCapDe(db.Model, GetCap):
-    __tablename__ = 'view_bod_wmts_getcapabilities_de'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_de'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
     query_class = QueryGetCap
 
 
 class GetCapEn(db.Model, GetCap):
-    __tablename__ = 'view_bod_wmts_getcapabilities_en'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_en'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
     query_class = QueryGetCap
 
 
 class GetCapIt(db.Model, GetCap):
-    __tablename__ = 'view_bod_wmts_getcapabilities_it'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_it'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
     query_class = QueryGetCap
 
 
 class GetCapRm(db.Model, GetCap):
-    __tablename__ = 'view_bod_wmts_getcapabilities_rm'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_rm'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
     query_class = QueryGetCap
 
 
@@ -97,91 +87,87 @@ class GetCapThemes(object):
     id = db.Column('inspire_id', db.Unicode, primary_key=True)
     inspire_name = db.Column('inspire_name', db.Unicode)
     inspire_abstract = db.Column('inspire_abstract', db.Unicode)
-    inspire_upper_theme_name = db.Column('inspire_oberthema_name', db.Unicode)
-    upper_theme_id = db.Column('oberthema_id', db.Unicode)
+    inspire_upper_theme_name = db.Column('inspire_upper_theme_name', db.Unicode)
+    inspire_upper_theme_id = db.Column('inspire_upper_theme_id', db.Unicode)
     inspire_upper_theme_abstract = db.Column(
-        'inspire_oberthema_abstract', db.Unicode
+        'inspire_upper_theme_abstract', db.Unicode
     )
-    fk_dataset_id = db.Column('fk_dataset_id', db.Unicode)
+    fk_dataset_ids = db.Column('fk_dataset_ids', db.ARRAY(db.Unicode))
 
 
 class GetCapThemesFr(db.Model, GetCapThemes):
-    __tablename__ = 'view_bod_wmts_getcapabilities_themes_fr'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_themes_fr'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class GetCapThemesDe(db.Model, GetCapThemes):
-    __tablename__ = 'view_bod_wmts_getcapabilities_themes_de'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_themes_de'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class GetCapThemesIt(db.Model, GetCapThemes):
-    __tablename__ = 'view_bod_wmts_getcapabilities_themes_it'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_themes_it'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class GetCapThemesRm(db.Model, GetCapThemes):
-    __tablename__ = 'view_bod_wmts_getcapabilities_themes_rm'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_themes_rm'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class GetCapThemesEn(db.Model, GetCapThemes):
-    __tablename__ = 'view_bod_wmts_getcapabilities_themes_en'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_getcapabilities_themes_en'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class ServiceMetadata(object):
-    id = db.Column('wms_id', db.Unicode, primary_key=True)  # pylint: disable=invalid-name
+    id = db.Column('wms_id', db.Unicode, primary_key=True)
     pk_map_name = db.Column('pk_map_name', db.Unicode)
     title = db.Column('title', db.Unicode)
-    onlineresource = db.Column('onlineresource', db.Unicode)
     abstract = db.Column('abstract', db.Unicode)
     keywords = db.Column('keywords', db.Unicode)
     fee = db.Column('fee', db.Unicode)
-    access_constraint = db.Column('accessconstraint', db.Unicode)
-    encoding = db.Column('encoding', db.Unicode)
+    access_constraint = db.Column('access_constraint', db.Unicode)
     fk_contact_id = db.Column('fk_contact_id', db.Integer)
-    address_type = db.Column('addresstype', db.Unicode)
+    address_type = db.Column('address_type', db.Unicode)
     address = db.Column('address', db.Unicode)
     postcode = db.Column('postcode', db.Integer)
     city = db.Column('city', db.Unicode)
     country = db.Column('country', db.Unicode)
-    contact_email_address = db.Column(
-        'contactelectronicmailaddress', db.Unicode
-    )
-    contact_person = db.Column('contactperson', db.Unicode)
-    contact_phone = db.Column('contactvoicetelephon', db.Unicode)
-    state_or_province = db.Column('stateorprovince', db.Unicode)
+    contact_email_address = db.Column('contact_email_address', db.Unicode)
+    contact_person = db.Column('contact_person', db.Unicode)
+    contact_phone = db.Column('contact_phone', db.Unicode)
+    state_or_province = db.Column('state_or_province', db.Unicode)
     fk_contact_organisation_id = db.Column(
-        'fk_contactorganisation_id', db.Integer
+        'fk_contact_organisation_id', db.Integer
     )
-    abbreviation = db.Column('abkuerzung', db.Unicode)
+    abbreviation = db.Column('abbreviation', db.Unicode)
     name = db.Column('name', db.Unicode)
 
 
 class ServiceMetadataDe(db.Model, ServiceMetadata):
-    __tablename__ = 'view_wms_service_metadata_de'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_service_metadata_de'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class ServiceMetadataFr(db.Model, ServiceMetadata):
-    __tablename__ = 'view_wms_service_metadata_fr'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_service_metadata_fr'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class ServiceMetadataIt(db.Model, ServiceMetadata):
-    __tablename__ = 'view_wms_service_metadata_it'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_service_metadata_it'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class ServiceMetadataRm(db.Model, ServiceMetadata):
-    __tablename__ = 'view_wms_service_metadata_rm'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_service_metadata_rm'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 class ServiceMetadataEn(db.Model, ServiceMetadata):
-    __tablename__ = 'view_wms_service_metadata_en'
-    __table_args__ = ({'schema': 're3', 'autoload': False})
+    __tablename__ = 'view_wmts_service_metadata_en'
+    __table_args__ = ({'schema': 'service-wmts', 'autoload': False})
 
 
 localized_models = {
